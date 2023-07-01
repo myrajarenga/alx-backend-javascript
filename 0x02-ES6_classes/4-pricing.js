1f5cbbe6fe0c065a7266c3e11c96f19ed00a5994
root <myrajarenga1234@gmail.com>
2023-07-01 15:45:06 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = typeof amount === 'number' ? amount : 0;
+    this._currency = currency instanceof Currency ? currency : new Currency('', '');
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    return NaN; // Return NaN if the inputs are not valid numbers
+  }
+}
