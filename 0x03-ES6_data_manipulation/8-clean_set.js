0652a586336e9c41008c08458606df53ddfe1e29
root <myrajarenga1234@gmail.com>
2023-07-06 13:33:18 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,16 @@
 export default function cleanSet(set, startString) {
-  const filterValues = Array.from(set).filter((value) => value.startsWith(startString));
-  const newString = filterValues.join('-');
-  return newString;
+  let result = '';
+
+  set.forEach((value) => {
+    const stringValue = String(value);
+    if (stringValue.startsWith(startString)) {
+      const restOfString = stringValue.slice(startString.length);
+      if (result.length > 0) {
+        result += '-';
+      }
+      result += restOfString;
+    }
+  });
+
+  return result;
 }
